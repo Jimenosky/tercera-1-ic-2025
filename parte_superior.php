65fd7e2dc8fb298cdeaae3f3e94dbc3940a196cd
Jimena <afjimena25@gmail.com>
2025-02-17 22:10:54 -0600

    Menu

---
+++ parte_superior.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="Spanish"> 
+
+<head>
+    <meta charset="utf-8">
+    <meta name="Proyecto Tercera" content="Proyecto Tercera">
+    <link rel="stylesheet" href="./css/main.css">
+</head>
+
+<menu>
+    <div class="menu-up">
+        <img src="./img/wowhead.webp" alt="">
+    </div>
+
+    <div class="menu-down">
+
+        <div class="menu-left">
+            <a href="">News</a>
+            <a href="">Discord Webhook</a>
+            <a href="">Database</a>
+            <a href="">Tools</a>
+            <a href="">Guides</a>
+            <a href="">Community</a>
+            <a href="">Premium</a>
+        </div>
+
+        <div class="menu-right">
+
+        </div>
+
+    </div>
+</menu>
